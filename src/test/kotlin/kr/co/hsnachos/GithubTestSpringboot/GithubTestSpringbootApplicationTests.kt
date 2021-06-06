c6d574dfa60b5672d2f33c6eb6a6bd99838645cf
hsnachos <hsnachos@gmail.com>
2021-06-06 11:31:45 +0900

    spring boot first commit

---
+++ src/test/kotlin/kr/co/hsnachos/GithubTestSpringboot/GithubTestSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.hsnachos.GithubTestSpringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubTestSpringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
